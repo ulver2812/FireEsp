4f6f2f4195a8f5045e234ee40e095cf94da8a19a
Init <developers.init.io@gmail.com>
2025-01-24 14:37:37 +0600

    Update FbServer.h

--- src/FbServer.h
+++ src/FbServer.h
@@ -3,10 +3,13 @@
 
 #include <Arduino.h>
 
+// Define DEBUG for enabling/disabling debug prints
+#define DEBUG 1 // Set to 0 for production, 1 for debugging
+
 class FbServer {
 public:
     // Constructor
-    FbServer(String apiKey, String authDomain, String databaseURL);
+    FbServer(const String& apiKey, const String& authDomain, const String& databaseURL);
 
     // Public Methods
     void initialize();
@@ -21,6 +24,9 @@ private:
     String _apiKey;
     String _authDomain;
     String _databaseURL;
+
+    // Debug helper method
+    void debugPrint(const String& message) const;
 };
 
 #endif // FBSERVER_H
